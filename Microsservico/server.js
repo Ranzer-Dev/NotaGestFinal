2ca759d52835c47e8018ce8ec7221b6bfd730b23
Ranzer_Dev <rodolfo.antunes9@gmail.com>
2025-10-22 21:41:15 -0300

    versel

--- Microsservico/server.js
+++ Microsservico/server.js
@@ -3,9 +3,9 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('microsservico/config/mongoDb.js'); 
+const connectDB = require('./config/mongoDb'); 
 
-const authRoutes = require('microsservico/routes/authRoutes'); 
+const authRoutes = require('./routes/authRoutes'); 
 
 dotenv.config();
 
